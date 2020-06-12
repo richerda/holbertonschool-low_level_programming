b887a8676f78dc2ab7acca19ba9d0acaa88d37f4
richerda <1900@holbertonschool.com>
2020-06-12 04:18:36 +0000

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *main - entry
+ *
+ *Definition: program tha print the numbers 1-10
+ *
+ *
+ *Return: 0 (success)
+ */
+int main(void)
+{
+int n;
+for (n = 0; n < 10; ++n)
+printf("%d", n);
+putchar('\n');
+return (0);
+}
