f55dbff8800847c6b1af061a2226ed3e9616af04
richerda <1900@holbertonschool.com>
2020-06-12 04:26:16 +0000

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *main - entry
+ *
+ *Description: program that prints digits from 0 to 9
+ *
+ *Return: 0 (success)
+ */
+int main(void)
+{
+int n;
+for (n = 48; n <= 57; ++n)
+putchar(n);
+putchar('\n');
+return (0);
+}
