1f7ae279bf65bf8fd7c1a8f38eee5ee7255b3594
richerda <1900@holbertonschool.com>
2020-06-16 03:34:25 +0000

    add _putchar

---
+++ 0-holberton.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ *main - entry
+ *
+ *Description: description
+ *
+ *Return: 0 (success)
+ */
+int main(void)
+{
+  char a[9] = "Holberton";
+  int j;
+  for (j = 0; j <= 8; j++)
+    _putchar(a[j]);
+  _putchar('\n');
+  return (0);
+}
